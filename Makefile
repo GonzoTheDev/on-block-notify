725222a74d5899b054395afc09e39e7d9474289f
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2019-12-27 13:26:12 -0500

    add windows support

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ all: notify
 notify:
 	gcc notify.c -o notify 
 
+notify_win:
+	gcc notify_win.c -o notify_win.exe -lwsock32
---
+++ notify_win.c
@@ -0,0 +1,42 @@
+#include <winsock2.h>
+#include <stdio.h> 
+#define MAX 80 
+#define PORT 14651
+
+int main() 
+{ 
+	WSADATA wsa;
+	SOCKET s;
+	struct sockaddr_in server;
+	
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		exit(0); 
+	}
+	
+	if((s = socket(AF_INET, SOCK_STREAM, 0 )) == INVALID_SOCKET)
+	{
+		printf("socket creation failed...\n");
+	}
+	
+	server.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	server.sin_family = AF_INET; 
+	server.sin_port = htons(PORT); 
+	
+	if (connect(s, (struct sockaddr*) &server, sizeof(server)) < 0)
+	{
+		printf("connection with the server failed...\n");
+		exit(0);
+	}
+	else
+	{
+		printf("trigger block reload\n");
+	}
+
+	const char* buff = "reload\n";
+	send(s , buff , strlen(buff) , 0);
+
+	closesocket(s); 
+	WSACleanup();
+} 
